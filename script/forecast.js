82738039b1bb8841747bbb819b5bd280cf169f52
Mary <maryastevens20@gmail.com>
2021-10-13 15:40:43 -0400

    add html, js

---
+++ script/forecast.js
@@ -0,0 +1,37 @@
+
+const key = '9HlKRDeGD4cYkzacaYU7dEvMAyO7Rkv3';
+
+//gets weather information
+const getWeather = async (id) => {//location id
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;//location id is from location key 
+  
+  const response = await fetch(base + query);//await the promise until it resolves
+  const data = await response.json();
+  return data[0]; 
+  
+};
+
+//gets city information
+const getCity = async (city) => {
+  
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+  //? adds a query parameter
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  //console.log(data[0]);//add [0] for the closest city match
+
+  return data[0];
+
+};
+
+getCity('tokyo').then(data => {//gets the location key to get the weather in location
+    return getWeather(data.Key);
+  }).then(data => {
+    console.log(data);
+  }).catch(error => console.log(error));
+
